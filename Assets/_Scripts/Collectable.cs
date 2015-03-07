743351db52e495f6583692f046adecd9af38daf5
Nbalsh <brandon.bn.nelson@gmail.com>
2015-03-06 23:18:55 -0800

    Collectable Script
    Here is a collectable Script

---
+++ Assets/_Scripts/Collectable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class Collectable : MonoBehaviour {
+
+	private CardboardHead head;
+	private Vector3 startingPosition;
+	private float delay = 2.0f; 
+
+	// Use this for initialization
+	void Start () {
+		head = Camera.main.GetComponent<StereoController>().Head;
+		startingPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+		GetComponent<Renderer>().material.color = isLookedAt ? Color.green : Color.red;
+
+		if ((Cardboard.SDK.CardboardTriggered && isLookedAt) || (Time.time>delay && isLookedAt) ) {
+			// Do Stuff black if looked at for now
+			GetComponent<Renderer>().material.color = isLookedAt ? Color.black : Color.red;
+		}
+	}
+}
