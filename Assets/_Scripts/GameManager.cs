36c68f5d5b452c141602041f391c531bba4f725b
krytical <johnnie-l@Live.ca>
2015-03-06 22:57:32 -0800

    Orb spawn

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	SpawnSpot[] spawnSpots;
+	public GameObject orb;
+	// Use this for initialization
+	void Start () {
+		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot> ();
+		SpawnOrbs ();
+		Debug.Log ("Started");
+	}
+
+
+	void SpawnOrbs() {
+		if (spawnSpots == null) {
+			Debug.LogError ("No Spawn Spots");
+			return;
+		}
+		Debug.Log ("In SpawnOrbs");
+		SpawnSpot orbSpawnSpot = spawnSpots [Random.Range (0, spawnSpots.Length)];
+		Debug.Log ("Created Orb");
+		orb = (GameObject)Instantiate (orb, orbSpawnSpot.transform.position, orbSpawnSpot.transform.rotation);
+	}
+}
---
+++ Assets/_Scripts/SpawnSpot.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnSpot : MonoBehaviour {
+	public int objID = 0;
+}
