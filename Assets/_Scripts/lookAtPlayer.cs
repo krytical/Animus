a2631f11703a423f8d74ff00e89c48593fa9163a
Nbalsh <brandon.bn.nelson@gmail.com>
2015-03-16 23:19:24 -0700

    Demo scene added, needs more work
    Demo scene added, needs more work

--- Assets/_Scripts/lookAtPlayer.cs
+++ Assets/_Scripts/lookAtPlayer.cs
@@ -21,5 +21,9 @@ public class lookAtPlayer : MonoBehaviour {
 		transform.LookAt( Camera.main.transform.position - v );
 		
 		transform.rotation =(Camera.main.transform.rotation);
+		if (transform.position.y <= 2.0f) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y + 1.0f, transform.position.z);
+		}
+		
 	}
 }
